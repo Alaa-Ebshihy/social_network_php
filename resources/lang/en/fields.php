5cabd92e955745895be6def8a105ae6316c1220f
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2019-01-15 13:33:44 +0200

    Added signup/in page view.

---
+++ resources/lang/en/fields.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	//Common key
+    'welcome_title' => 'Larvel simple social network',
+    'welcome_header' => 'Simple social network',
+
+	//keys for login and signup page
+	'signup' => 'Sign Up',
+	'signin' => 'Sign In',
+	'email' => 'E-mail',
+	'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'user_name' => 'User Name',
+	'password' => 'Password',
+	'submit' => 'Submit',
+];
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <h1 class="navbar-brand">@lang('fields.welcome_header')</h1>
+	</nav>
+</header>
